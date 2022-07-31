301e92e3eb72c1bfe1d96ada9966d23fc0f458a0
Golban-Julia <golden95juli@gmail.com>
2022-07-31 18:37:23 +0300

    Adder two case

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const name = prompt( "Как вас зовут?", '');
+alert(`Ваше имя ${name}`);
+
+
+let a = 15;
+let b = 10;
+
+let sum = a + b;
+alert (sum);
+
+let sub = a - b;
+alert (sub);
+
+let multiply = a * b;
+alert(multiply);
+
+let division = a / b;
+alert (division);
+
+let exponent = a ** b;
+alert (exponent);
+
